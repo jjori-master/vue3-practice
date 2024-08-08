95f450c57aa723259a86ba76715d6dc853e951c2
jjori <jjori.master@gmail.com>
2024-08-09 00:33:38 +0900

    vue 기본 제공 코드 제거

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App).mount("#app");
+
+app.config.errorHandler = (err, vm, info) => {
+  console.error(err, vm, info);
+  debugger;
+};
